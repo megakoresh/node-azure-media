775b274350ebf4c9ab6b19c0fb34fc7449b54dff
megakoresh <megakoresh@gmail.com>
2015-11-02 11:15:20 +0200

    Added a function to get image thumbnails (or really any images inside the asset)

--- media.js
+++ media.js
@@ -244,7 +244,21 @@ function AzureBlob(api) {
       this.getUrl(assetId, duration, 2, done_cb);
     };    
 
-    this.encodeVideo = function (assetId, mediaProcessor, encoder, callback) {
+    this.getThumbnails = function(assetId, duration, done_cb){
+	if(arguments.length == 2) {
+		done_cb = arguments[1];
+		duration = 1440;
+	}
+	this.getUrl(assetId, duration, 1, function(err, path, thumbnails){
+		if(!err && thumbnails && thumbnails.length>0){
+			return done_cb(null, thumbnails)
+		} else {
+			return done_cb(err || 'No thumbnails found.');
+		}
+	});
+	}
+	
+	this.encodeVideo = function (assetId, mediaProcessor, encoder, callback) {
         async.waterfall([
             function (cb) {
                 this.api.rest.mediaprocessor.getCurrentByName(mediaProcessor, cb);
